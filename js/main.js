b985e6b5e4a666ba2c10615f83ee4f510541bbc7
Sebastian Śliwiński <sxsliwinski@gmail.com>
2018-03-21 18:12:15 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 document.getElementById('oblicz').addEventListener('click', function () {
     var czas = document.getElementsByClassName('czas');
     var stawka = document.getElementsByClassName('stawka');
+    var array = [];
 
-    for (var i = 0; i < czas.length && czas.length; i++) {
+    for (var i = 0; i < czas.length; i++) {
         var wyplata = czas[i].defaultValue * stawka[i].defaultValue;
         document.getElementsByClassName('wyplata')[i].innerHTML = wyplata;
+
+        array.push(czas[i].value);
 
         if (czas[i].defaultValue > 160) {
             var iloscNadgodzin = czas[i].defaultValue - 160;
@@ -13,8 +16,24 @@ document.getElementById('oblicz').addEventListener('click', function () {
         } else if (czas[i].defaultValue < 100) {
             document.getElementsByClassName('pracownik')[i].style.backgroundColor = 'red';
         }
-        var array = [];
-        array.push(parseInt(czas[i].defaultValue));
-    } 
-    console.log(array);
+    }
+    var arraySorted = array.sort(function (e1, e2) {
+        return e1 - e2;
+    });
+    var arraySorted = arraySorted.reverse();
+
+    for (var i = 0; i < czas.length; i++) {
+        if (arraySorted[0] === czas[i].value) {
+            var nameFirst = document.getElementsByClassName('pracownik')[i].innerHTML;
+        } else if (arraySorted[1] === czas[i].value) {
+            var nameSecond = document.getElementsByClassName('pracownik')[i].innerHTML;
+        } else if (arraySorted[2] === czas[i].value) {
+            var nameThird = document.getElementsByClassName('pracownik')[i].innerHTML;
+        }
+    }
+    var ul = document.getElementById('najlepsi_pracownicy');
+    var li = document.createElement('li');
+
+    ul.appendChild(li);
+    li.innerHTML = nameFirst + '<li>' + nameSecond + '<li>' + nameThird;
 });
